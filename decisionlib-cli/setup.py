c96fa26f52872a02e743da3b32a66d56e7e3e1cc
Mitchell Hentges <mhentges@mozilla.com>
2019-04-22 21:12:11 +0200

    decisionlib-cli 0.0.2

--- decisionlib-cli/setup.py
+++ decisionlib-cli/setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='decisionlib-cli-mhentges',
-    version='0.0.1',
+    version='0.0.2',
     author='Mozilla Release Engineering',
     author_email='release+python@mozilla.com',
     description='Taskcluster task-builder',
