3e5a2bbabf64c09155d438909ad993c6e625f71d
Mitchell Hentges <mhentges@mozilla.com>
2019-04-24 17:30:28 +0200

    0.0.29

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='decisionlib-mhentges',
-    version='0.0.28',
+    version='0.0.29',
     author='Mozilla Release Engineering',
     author_email='release+python@mozilla.com',
     description='Taskcluster task-builder',
